8832b5dd53b7bc267b0d39a3a230169a80bf4608
jason <liaowuhen821@126.com>
2017-05-27 11:35:58 +0800

    bus

---
+++ config-client-eureka-bus/src/main/java/com/jason/springcloud/ConfigClientEurekaBusApplication.java
@@ -0,0 +1,28 @@
+package com.jason.springcloud;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope  //用途不知，目前没啥用
+@EnableEurekaClient
+@RestController
+@SpringBootApplication
+public class ConfigClientEurekaBusApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigClientEurekaBusApplication.class, args);
+    }
+
+    @Value("${foo}")
+    String foo;
+
+    @RequestMapping(value = "/hi")
+    public String hi() {
+        return foo;
+    }
+}
